7c869503f7d60b7150d18e02cc7e848b6a12665a
Atharva Metkar <avm5998@gmail.com>
2023-12-02 13:56:24 -0500

    day2

---
+++ cubeconundrum.py
@@ -0,0 +1,49 @@
+
+
+ans = 0
+with open("data2.txt", "r") as file:
+    lines = file.readlines()
+    linenumber = 1
+    check = {'red': 12, 'green': 13, 'blue': 14}
+    for line in lines:
+        templine = line.split(':')[1].strip()
+        turns = templine.strip().split(';')
+        flag = True
+        for turn in turns:
+            temp = turn.strip().split(' ')
+            for i in range(0, len(temp), 2):
+                freq = int(temp[i])
+                color = temp[i+1]
+                if i < len(temp)-2:
+                    color = color[:-1]
+                if freq > check[color]:
+                    flag = False
+                    break
+        if flag:
+            ans += linenumber
+        linenumber += 1
+    print("ans is " + str(ans))
+
+# part two
+ans = 0
+with open("data2.txt", "r") as file:
+    lines = file.readlines()
+    for line in lines:
+        r = g = b = 0
+        templine = line.split(':')[1].strip()
+        turns = templine.strip().split(';')
+        for turn in turns:
+            temp = turn.strip().split(' ')
+            for i in range(0, len(temp), 2):
+                freq = int(temp[i])
+                color = temp[i+1]
+                if i < len(temp)-2:
+                    color = color[:-1]
+                if color == 'red':
+                    r = max(r, freq)
+                elif color == 'green':
+                    g = max(g, freq)
+                else:
+                    b = max(b, freq)
+        ans += r*g*b
+    print("ans is " + str(ans))
--- trebuchet.py
+++ trebuchet.py
@@ -1,38 +1,41 @@
 
 
-ans=0
-with open('data1.txt','r') as f:
-    lines=f.readlines()
-    first=last=None
-    numbers={'one':1,'two':2,'three':3,'four':4,'five':5,'six':6,'seven':7,'eight':8,'nine':9}
+ans = 0
+with open('data1.txt', 'r') as f:
+    lines = f.readlines()
+    first = last = None
+    numbers = {'one': 1, 'two': 2, 'three': 3, 'four': 4,
+               'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9}
     for line in lines:
-        n=len(line)
-        i,j=0,n-1
-        chosen=None
-        while i<n:
-            if line[i].isnumeric(): 
-                chosen=int(line[i])
-            elif i+2<n and line[i:i+3] in numbers:
-                chosen=numbers[line[i:i+3]]
-            elif i+3<n and line[i:i+4] in numbers:
-                chosen=numbers[line[i:i+4]]
-            elif i+4<n and line[i:i+5] in numbers:
-                chosen=numbers[line[i:i+5]]
-            if chosen is not None: break
-            i+=1
-        nextno=chosen
-        chosen=None
-        i=n-1
-        while i>=0:
-            if line[i].isnumeric(): 
-                chosen=int(line[i])
-            elif i+2<n and line[i:i+3] in numbers:
-                chosen=numbers[line[i:i+3]]
-            elif i+3<n and line[i:i+4] in numbers:
-                chosen=numbers[line[i:i+4]]
-            elif i+4<n and line[i:i+5] in numbers:
-                chosen=numbers[line[i:i+5]]
-            if chosen is not None: break
-            i-=1
-        ans+=10*nextno+chosen
+        n = len(line)
+        i, j = 0, n-1
+        chosen = None
+        while i < n:
+            if line[i].isnumeric():
+                chosen = int(line[i])
+            elif i+2 < n and line[i:i+3] in numbers:
+                chosen = numbers[line[i:i+3]]
+            elif i+3 < n and line[i:i+4] in numbers:
+                chosen = numbers[line[i:i+4]]
+            elif i+4 < n and line[i:i+5] in numbers:
+                chosen = numbers[line[i:i+5]]
+            if chosen is not None:
+                break
+            i += 1
+        nextno = chosen
+        chosen = None
+        i = n-1
+        while i >= 0:
+            if line[i].isnumeric():
+                chosen = int(line[i])
+            elif i+2 < n and line[i:i+3] in numbers:
+                chosen = numbers[line[i:i+3]]
+            elif i+3 < n and line[i:i+4] in numbers:
+                chosen = numbers[line[i:i+4]]
+            elif i+4 < n and line[i:i+5] in numbers:
+                chosen = numbers[line[i:i+5]]
+            if chosen is not None:
+                break
+            i -= 1
+        ans += 10*nextno+chosen
     print(ans)
